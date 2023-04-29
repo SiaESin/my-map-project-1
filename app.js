ce0b76454e3342746b7a283183c22f084f40d977
SiaESin <siabhon.sindab@gmail.com>
2023-04-29 12:01:20 -0700

    working map

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 console.log('hello lovely lady');
 //create map
+var map = L.map('map').setView([51.505, -0.09], 13);
 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 //API: geolocation
-async function getCoords(){
-    pos = await new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject)
-    })
-    return [pos.coords.latitude, pos.coords.longitude]
-}
+// async function getCoords(){
+//     pos = await new Promise((resolve, reject) => {
+//         navigator.geolocation.getCurrentPosition(resolve, reject)
+//     })
+//     return [pos.coords.latitude, pos.coords.longitude]
+// }
 
-console.log(getCoords());                              
+// console.log(getCoords());                              
 
 //foursquare API. If more consider the order
 

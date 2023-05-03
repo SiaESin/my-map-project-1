8308ebb37dddf2806e8e1b046b409bace58092a8
SiaESin <siabhon.sindab@gmail.com>
2023-05-03 12:29:00 -0700

    code corrections made, drop down form added

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
-console.log('hello lovely lady');
-//create map
-var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+
+
+
+  
+
+
+//create map
+const myMap ={ 
+        coordinates:[],
+        businesses: [],
+	    map: {},
+	    markers: {},
+
+        createMap(){
+            this.map = L.map('map', {
+                center: this.coordinates,
+                zoom: 16,
+            })
+//how to add user's location
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>contributors',
+            }).addTo(this.map)
+            const marker = L.marker(this.coordinates).addTo(this.map)
+            marker.bindPopup("You are here! Yes, we see you... haha").openPopup()
+
+        }
+}
 
 //API: geolocation
-// async function getCoords(){
-//     pos = await new Promise((resolve, reject) => {
-//         navigator.geolocation.getCurrentPosition(resolve, reject)
-//     })
-//     return [pos.coords.latitude, pos.coords.longitude]
-// }
-
-// console.log(getCoords());                              
-
+async function getCoords(){
+    pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+    return [pos.coords.latitude, pos.coords.longitude]
+}
+console.log(getCoords());
 //foursquare API. If more consider the order
 
-//how to add user's location
+//eventhandlers 
+window.onload = async () => {
+    const coords = await getCoords()
+    console.log(coords)
+    myMap.coordinates = coords
+    myMap.createMap()
+}
 
 //need other locations 
 
